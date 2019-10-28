c04730ddd83ff33bb028afa42fe65de4416a8939
Action Trainee <actiontrainee@devg-cpu-0916s-macbook-pro.local>
2019-10-28 16:15:16 +0800

    Initial Commit

---
+++ KaoHon/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KaoHon
+//
+//  Created by Action Trainee on 28/10/2019.
+//  Copyright © 2019 Action Trainee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
